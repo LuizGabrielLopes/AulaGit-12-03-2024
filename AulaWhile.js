f2290905ba69084cc1f46b7c0d0250d933bbc6eb
Luiz Gabriel <luizgabriellopesc@gmail.com>
2024-03-12 08:26:29 -0300

    Adição do arquivo AulaWhile.js

---
+++ AulaWhile.js
@@ -0,0 +1,20 @@
+let a = 10;
+
+let b = 20;
+
+let inicio;
+
+let fim;
+
+if (a < b) {
+    inicio = a;
+    fim = b;
+} else {
+    inicio = b;
+    fim = a;
+}
+
+while (inicio <= fim) {
+    console.log(inicio);
+    inicio++;
+}
